49ddcacf1d48ccd929a0b4231a4553ae687f4576
loggersl <juansgarbagedisosal@gmail.com>
2025-12-06 12:41:11 +0000

    Add files via upload

---
+++ EvenGeneratorV5.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+// This is the final* version. There was literally 0 need for me to make this. Have fun :3
+void createfile(int amount, std::string path){
+    char inversecomma = '"';
+    std::ofstream file(path);
+    
+    file << "x = int(input('> '))\n";
+    file << "if x == 0:\n   print('even')";
+
+
+    for(int i = 1; i < int(amount+1); i++){
+        std::string statement = "";
+        if(i % 2 == 0) {
+            file << "\nelif x == " + std::to_string(i) + ":\n  print('even')";}
+        else {
+            file << "\nelif x == " + std::to_string(i) + ":\n  print('odd')";}
+        }
+    file.close();
+}
+int main() {
+    createfile(1000000000,"example.py");
+    return 0;
+}
